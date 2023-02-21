0a03445f7032cac5315e3be2bc7152a35aefdd14
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2023-02-21 19:34:26 -0300

    ll

--- webapp/js/dinamica.js
+++ webapp/js/dinamica.js
@@ -1,34 +1,26 @@
 
 
     const controls = document.querySelectorAll(".arrow");
-    let currentItem = 0;
-    const items = document.querySelectorAll(".card");
-    const maxItems = items.length;
-
-  
+   
 
     controls.forEach((control) => {
         control.addEventListener("click", (e) => {
         
         isLeft = e.target.classList.contains("fa-arrow-left");
 
-        //console.log(e)
-
-
-        if (isLeft && currentItem >0) {
-            currentItem -= 1;
-            } else if(!isLeft && currentItem < maxItems -1) {
-            currentItem += 1;
-            }
-        
-        console.log(currentItem)
-       
-
-        items[currentItem].scrollIntoView({
-        behavior: "smooth",
-        block: 'nearest',
-        inline: "center"
-        });
+        if (isLeft) {
+            document.getElementById('box-card').scrollBy({
+                top: 0,
+                left: -220,
+                behavior: 'smooth'
+              });
+        } else{
+            document.getElementById('box-card').scrollBy({
+                top: 0,
+                left: 220,
+                behavior: 'smooth'
+              });
+        }
 
        
     });
