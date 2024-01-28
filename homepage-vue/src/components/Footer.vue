6a342ac2a2b3bc43293bb8d7b84a3f495db6aca6
YuriNaoki <dev.peterson.machado@gmail.com>
2024-01-28 14:31:58 -0300

    Adicionando Bootstrap

---
+++ homepage-vue/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer bg-primary text-center text-lg-start">
+        <div class="container p-4">
+            <p>
+                <strong>Seu projeto</strong> por <a href="https://seusite.com">Seu Nome</a>.
+                O código fonte está lincenciado
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
--- homepage-vue/src/main.js
+++ homepage-vue/src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 const app = createApp(App)
 
 app.use(router)
-app.use(BootstrapVue)
-app.use(IconsPlugin)
 
 app.mount('#app')
